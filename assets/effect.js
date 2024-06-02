74295f556db2f39ead84d9a899d93dc4a718d23d
JayPatel <108858654+jaypatel364@users.noreply.github.com>
2024-06-03 00:02:21 +0530

    Update effect.js

--- assets/effect.js
+++ assets/effect.js
@@ -176,10 +176,10 @@ $('document').ready(function(){
 		$('#b7').attr('id','b77')
 		$('#b11').animate({top:240, left: vw-350},500);
 		$('#b22').animate({top:240, left: vw-250},500);
-		$('#b33').animate({top:240, left: vw-150},500);
-		$('#b44').animate({top:240, left: vw-50},500);
-		$('#b55').animate({top:240, left: vw+50},500);
-		$('#b66').animate({top:240, left: vw+150},500);
+		$('#b33').animate({top:240, left: vw-180},500);
+		$('#b44').animate({top:240, left: vw-100},500);
+		$('#b55').animate({top:240, left: vw+0},500);
+		$('#b66').animate({top:240, left: vw+100},500);
 		$('#b77').animate({top:240, left: vw+250},500);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
